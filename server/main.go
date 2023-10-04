1546ce574618f582567d994ba4d05f563d975fc1
jpinn97 <JP878@canterbury.ac.uk>
2023-10-04 17:54:21 +0100

    Route handlers for item options. #1 #2 #3 #4 #5.

--- server/main.go
+++ server/main.go
@@ -14,6 +14,18 @@ func main() {
 	// Define route handler at root.
 	r.GET("/", RootHandler)
 
+	// Define route handler for POST a single item.
+	r.GET("/item/", POSTSingleItemHandler)
+
+	// Define route handler for GET a single item.
+	r.GET("/item/:itemId/", GETSingleItemHandler)
+
+	// Define route handler for DELETE a single item.
+	r.GET("/item/:itemId/", DELETESingleItemHandler)
+
+	// Define route handler for GET multiple items.
+	r.GET("/items/", GETMultipleItemsHandler)
+
 	// Publish on port 8000.
 	r.Run(":8000")
 }
@@ -23,4 +35,20 @@ func RootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello, World!",
 	})
+}
+
+func POSTSingleItemHandler(c *gin.Context) {
+
+}
+
+func GETSingleItemHandler(c *gin.Context) {
+
+}
+
+func DELETESingleItemHandler(c *gin.Context) {
+
+}
+
+func GETMultipleItemsHandler(c *gin.Context) {
+
 }
