69aa3a6edcbcc6897b6421823f14999320f8936c
jpinn97 <JP878@canterbury.ac.uk>
2023-10-04 17:35:08 +0100

    Dockerfile and Makefile implemented properly. - route handlers should be out of main for readability.

--- server/main.go
+++ server/main.go
@@ -8,13 +8,19 @@ import (
 
 func main() {
 	r := gin.Default()
+	/* Resources to build these functions is from
+	https://go.dev/doc/tutorial/web-service-gin */
 
-	// Define route handler at root
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, World!",
-		})
+	// Define route handler at root.
+	r.GET("/", RootHandler)
+
+	// Publish on port 8000.
+	r.Run(":8000")
+}
+
+// Handles HTTP requests to root.
+func RootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello, World!",
 	})
-
-	r.Run() // listen and serve on localhost:8080
 }
