bd78ec1b1f0f6cb97770b083eb68dbbb8566a433
jpinn97 <JP878@canterbury.ac.uk>
2023-10-04 22:34:15 +0100

    Implemented POST of single item with item schema. - Added atomic ID generator. - Added map mutex. - Both support concurrency of goroutines.

--- server/main.go
+++ server/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"net/http"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+// Item Struct is designed to be serialized to and deserialized from JSON. Note optional fields.
 type Item struct {
 	ID          int64      `json:"id"`
 	User_ID     string     `json:"user_id"`
@@ -19,8 +22,26 @@ type Item struct {
 	Date_to     *time.Time `json:"date_to,omitempty"`
 }
 
+// items map as package-level variable with mutex to ensure concurrency.
+var (
+	items   map[int64]Item
+	itemsMu sync.Mutex
+)
+
+func init() { // initialize map
+	items = make(map[int64]Item)
+}
+
+var idCounter int64 // initialise counter
+
+func generateUniqueID() int64 {
+	atomic.AddInt64(&idCounter, 1)      // Safely increment the counter
+	return atomic.LoadInt64(&idCounter) // Safely retrieve the counter value
+}
+
 func main() {
 	r := gin.Default()
+
 	/* Resources to build these functions is from
 	https://go.dev/doc/tutorial/web-service-gin */
 
@@ -28,16 +49,16 @@ func main() {
 	r.GET("/", RootHandler)
 
 	// Define route handler for POST a single item.
-	r.GET("/item/", POSTSingleItemHandler)
+	r.POST("/item", POSTSingleItemHandler)
 
 	// Define route handler for GET a single item.
-	r.GET("/item/:itemId/", GETSingleItemHandler)
+	r.GET("/item/:itemId", GETSingleItemHandler)
 
 	// Define route handler for DELETE a single item.
-	r.GET("/item/:itemId/", DELETESingleItemHandler)
+	r.DELETE("/item/:itemId", DELETESingleItemHandler)
 
 	// Define route handler for GET multiple items.
-	r.GET("/items/", GETMultipleItemsHandler)
+	r.GET("/items", GETMultipleItemsHandler)
 
 	// Publish on port 8000.
 	r.Run(":8000")
@@ -51,7 +72,22 @@ func RootHandler(c *gin.Context) {
 }
 
 func POSTSingleItemHandler(c *gin.Context) {
+	var newItem Item
 
+	// Call BindJSON to bind the received JSON to newItem.
+	if err := c.BindJSON(&newItem); err != nil {
+		c.JSON(http.StatusMethodNotAllowed, gin.H{
+			"error":       "Invalid input",
+			"description": "Some input fields may be missing",
+		})
+		return
+	}
+	newID := generateUniqueID()
+	// Add the new item to the locked items map.
+	itemsMu.Lock()
+	items[newID] = newItem
+	itemsMu.Unlock()
+	c.JSON(http.StatusCreated, gin.H{"description": "Item created successfully"})
 }
 
 func GETSingleItemHandler(c *gin.Context) {
