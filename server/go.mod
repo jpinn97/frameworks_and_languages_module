dff850541567c7f656e7a5fb653caa0e78d0f995
jpinn97 <JP878@canterbury.ac.uk>
2023-10-10 23:19:20 +0100

    Support for Deleting Item. Support for Getting multiple Item.

--- server/go.mod
+++ server/go.mod
@@ -2,11 +2,14 @@ module freecycle-inc_server
 
 go 1.21.1
 
+require github.com/umahmood/haversine v0.0.0-20151105152445-808ab04add26
+
 require (
 	github.com/bytedance/sonic v1.10.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/umahmood/haversine"
 )
 
 // Struct for Item schema that is POST to server.
@@ -18,7 +19,7 @@ type ItemPOST struct {
 	Description string   `json:"description" binding:"required"`
 	Image       *string  `json:"image,omitempty"`
 	Lat         float64  `json:"lat" binding:"required"`
-	Long        float64  `json:"long" binding:"required"`
+	Lon         float64  `json:"lon" binding:"required"`
 }
 
 // Struct for Item schema that is GET from server.
@@ -29,7 +30,7 @@ type Item struct {
 	Description string     `json:"description" binding:"required"`
 	Image       *string    `json:"image,omitempty"`
 	Lat         float64    `json:"lat" binding:"required"`
-	Long        float64    `json:"long" binding:"required"`
+	Lon         float64    `json:"lon" binding:"required"`
 	Date_from   time.Time  `json:"date_from" binding:"required"`
 	Date_to     *time.Time `json:"date_to,omitempty"`
 }
@@ -43,7 +44,7 @@ func ConvertItemPOSTToItems(itemPOST ItemPOST, id int64, dateFrom time.Time, dat
 		Description: itemPOST.Description,
 		Image:       itemPOST.Image,
 		Lat:         itemPOST.Lat,
-		Long:        itemPOST.Long,
+		Lon:         itemPOST.Lon,
 		Date_from:   dateFrom,
 		Date_to:     dateTo,
 	}
@@ -59,12 +60,7 @@ func init() { // initialize map
 	items = make(map[int64]Item)
 }
 
-var idCounter int64 // initialise counter
-
-func generateUniqueID() int64 {
-	atomic.AddInt64(&idCounter, 1)      // Safely increment the counter
-	return atomic.LoadInt64(&idCounter) // Safely retrieve the counter value
-}
+var idCounter int64 = 0 // initialise counter
 
 func main() {
 	r := gin.Default()
@@ -111,7 +107,6 @@ func POSTSingleItemHandler(c *gin.Context) {
 		return
 	}
 
-	newId := generateUniqueID() // Get Id.
 	currentTime := time.Now().UTC()
 	iso8601Time := currentTime.Format(time.RFC3339Nano)
 	parsedTime, err := time.Parse(time.RFC3339Nano, iso8601Time)
@@ -124,13 +119,16 @@ func POSTSingleItemHandler(c *gin.Context) {
 		return
 	}
 
+	newId := atomic.LoadInt64(&idCounter) // Safely read counter.
+	atomic.AddInt64(&idCounter, 1)        // Safetly increment counter.
+
 	StoredData := ConvertItemPOSTToItems(ReceivedData, newId, parsedTime, nil)
 
 	itemsMu.Lock() // Lock item mutex, thus locking the Items map.
 	defer itemsMu.Unlock()
 	items[newId] = StoredData
 
-	c.JSON(http.StatusCreated, gin.H{"description": "Item created successfully"})
+	c.JSON(http.StatusCreated, StoredData)
 }
 
 func GETSingleItemHandler(c *gin.Context) {
@@ -163,9 +161,129 @@ func GETSingleItemHandler(c *gin.Context) {
 }
 
 func DELETESingleItemHandler(c *gin.Context) {
+	itemId := c.Param("itemId") // Retrive itemId from request.
 
+	// Convert the itemID to an integer
+	id, err := strconv.ParseInt(itemId, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":       "Invalid item Id",
+			"description": "Item ID must be a valid integer",
+		})
+		return
+	}
+
+	itemsMu.Lock()
+	defer itemsMu.Unlock()
+
+	// Check if the item with the specified ID exists
+	_, exists := items[id]
+	if exists { // If true we delete.
+		delete(items, id)
+		c.JSON(http.StatusNoContent, nil)
+	} else { // If false we escape gracefully.
+		c.JSON(http.StatusNotFound, gin.H{
+			"error":       "Item not found",
+			"description": "No item with the specified ID",
+		})
+		return
+	}
+}
+
+/*
+As Go doesn't contains.. this built-in, inspired by: https://stackoverflow.com/a/10485970
+*/
+func contains(itemKeywords []string, arrayQueryKeywords []string) bool {
+	// Create a map to store the itemKeywords for faster lookup
+	keywordMap := make(map[string]bool)
+
+	// Populate the map with itemKeywords
+	for _, keyword := range itemKeywords {
+		keywordMap[keyword] = true
+	}
+
+	// Check if all query keywords are present in itemKeywords
+	for _, queryKeyword := range arrayQueryKeywords {
+		if _, found := keywordMap[queryKeyword]; !found {
+			// If a query keyword is not found in itemKeywords, return false
+			return false
+		}
+	}
+
+	// All query keywords were found in itemKeywords
+	return true
 }
 
 func GETMultipleItemsHandler(c *gin.Context) {
+	// Convert map values to a list (slice) of items.
+	var itemList []Item
+	for _, item := range items {
+		itemList = append(itemList, item)
+	}
+	if c.Request.URL.RawQuery == "" {
+		c.JSON(http.StatusOK, itemList)
+		return
+	} else {
+		// Retrieve and process query parameters if they exist.
+		user_id := c.Query("user_id")
+		keywords := c.QueryArray("keywords")
+		lat := c.Query("lat")
+		lon := c.Query("lon")
+		radius := c.DefaultQuery("radius", "5")
+		date_from := c.Query("date_from")
+		date_to := c.DefaultQuery("date_to", time.Now().UTC().Format(time.RFC3339Nano))
 
+		/* query algorithm
+
+		get key n = 1
+		if key exists, check key values
+		if any field contains values that matches to the query string,
+		given the defaults if none given, and +- 5 to the latitude/longitude,
+		take that key value add to slice1
+		n +=1 and check next key
+
+		*/
+
+		itemResults := []Item{}
+
+		for key, val := range items {
+			println(val.ID)
+			radius, err := strconv.ParseFloat(radius, 64)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, nil)
+				return
+			}
+			if val.User_ID == user_id {
+				itemResults = append(itemResults, items[key])
+				continue
+			}
+			if contains(val.Keywords, keywords) {
+				println(val.Keywords)
+				println(keywords)
+				itemResults = append(itemResults, items[key])
+				continue
+			}
+			// we wont perform validation on already stored lat/long, do at binding
+			lat, _ := strconv.ParseFloat(lat, 64)
+			lon, _ := strconv.ParseFloat(lon, 64)
+
+			// We use haversine packaged provided by: https://pkg.go.dev/github.com/umahmood/haversine
+			// to calculate the distance, i.e our radius
+			a := haversine.Coord{Lat: val.Lat, Lon: val.Lon}
+			b := haversine.Coord{Lat: lat, Lon: lon}
+			_, km := haversine.Distance(a, b)
+			if km <= radius {
+				itemResults = append(itemResults, items[key])
+				continue
+			}
+			date_from, _ := time.Parse(time.RFC3339Nano, date_from)
+			date_to, _ := time.Parse(time.RFC3339Nano, date_to)
+
+			if val.Date_from.Before(date_from) || (!val.Date_to.IsZero() && val.Date_to.After(date_to)) {
+				itemResults = append(itemResults, items[key])
+				continue
+			}
+		}
+		c.JSON(http.StatusOK, itemResults)
+	}
 }
