a445a89733b0572db9c429cd1e7ab8bb0f84c479
ABDUL RASHID OMENI <75305745+aromeni@users.noreply.github.com>
2023-10-28 16:05:34 +0000

    Implement methods for form submission, clearing form, and fetching items from server.

--- client/vue.js
+++ client/vue.js
@@ -3,16 +3,48 @@ const app = express()
 var cors = require('cors')
 const port = 8000;
 
-
+app.use(express.json());
 app.use(cors())
+
+
+let items = [];
+
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
+
 app.get('/', (req, res) => {
   res.sendFile('client.html', {root: __dirname})
 })
 
+
+
+app.post('/item', (req, res) => {
+  const item = req.body;
+  console.log('Received item:', item);
+
+  //item.id = Date.now();; // Using timestamp as a simple unique ID for this example.
+
+ items.push(item); // Store the new item in the items array
+  console.log('Received item again:', item);
+  res.json({ status: 'success', item });
+});
+
+
+app.get('/items', (req, res) => {
+  res.json(items); // sends the items array as a JSON response
+});
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Vue app listening on port ${port}`)
 })
+
+
+
+
+
 
