5d37414093a1bf43f96d46799d56ef49fefa3b0f
ABDUL RASHID OMENI <75305745+aromeni@users.noreply.github.com>
2023-11-09 21:36:51 +0000

    feat: Initialize project with Vite, React, and Tailwind CSS setup

---
+++ client_independent/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+      <div className="p-9 bg-blue-500 text-white font-bold rounded-lg">
+   <h1>Hello Rashid, Tailwind CSS!</h1>
+      </div>
+      
+
+  )
+}
+
+export default App
